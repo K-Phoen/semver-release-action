2bfb5d7b788caf16352310c791d7e8d62117a9bf
Kévin Gomez <contact@kevingomez.fr>
2019-09-29 09:45:08 +0200

    Better error handling for guards and increment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 set -u
 
-if [ -z "$GITHUB_TOKEN" ]
+if [ -z "${GITHUB_TOKEN}" ]
 then
     echo "The GITHUB_TOKEN environment variable is not defined."
     exit 1
@@ -11,13 +11,13 @@ fi
 
 RELEASE_BRANCH="$1"
 
-/bumper guard "$RELEASE_BRANCH" "$GITHUB_EVENT_PATH"
+/bumper guard "${RELEASE_BRANCH}" "${GITHUB_EVENT_PATH}"
 
-LATEST_TAG=$(/bumper latest-tag "$GITHUB_REPOSITORY" "$GITHUB_TOKEN")
+LATEST_TAG=$(/bumper latest-tag "${GITHUB_REPOSITORY}" "${GITHUB_TOKEN}")
 
-INCREMENT=$(/bumper increment "$GITHUB_EVENT_PATH")
-NEXT_TAG=$(/bumper semver "$LATEST_TAG" $INCREMENT)
+INCREMENT=$(/bumper increment "${GITHUB_EVENT_PATH}")
+NEXT_TAG=$(/bumper semver "${LATEST_TAG}" "${INCREMENT}")
 
-/bumper release "$GITHUB_REPOSITORY" "$GITHUB_SHA" "$NEXT_TAG" "$GITHUB_TOKEN"
+/bumper release "${GITHUB_REPOSITORY}" "${GITHUB_SHA}" "${NEXT_TAG}" "${GITHUB_TOKEN}"
 
-echo ::set-output name=tag::$NEXT_TAG
+echo ::set-output name=tag::${NEXT_TAG}
