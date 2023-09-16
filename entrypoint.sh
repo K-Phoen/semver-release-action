c5eec3116cd28470e43a0a4a54f1bbcd258b8eee
Ernyoke <ernyoke19930115@gmail.com>
2023-09-16 17:49:44 +0300

    Remove unnecessary GITHUB_URL.

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ GITHUB_BASE_HOST="$5"
 GITHUB_UPLOAD_HOST="$6"
 
 echo ::Executing bumper guard ::debug release_branch=${RELEASE_BRANCH},github_event_path=${GITHUB_EVENT_PATH}
-/bumper guard "${RELEASE_BRANCH}" "${GITHUB_EVENT_PATH}" "${GITHUB_URL}"
+/bumper guard "${RELEASE_BRANCH}" "${GITHUB_EVENT_PATH}"
 if [ $? -eq 78 ]
 then
     echo ::debug ::Guard returned a neutral code, stopping the execution.
@@ -26,10 +26,10 @@ fi
 if [ -z "${NEXT_TAG}" ]
 then
     echo ::debug ::Executing bumper latest-tag github_repository=${GITHUB_REPOSITORY}
-    LATEST_TAG=$(/bumper latest-tag "${GITHUB_REPOSITORY}" "${GITHUB_TOKEN}" "${GITHUB_URL}")
+    LATEST_TAG=$(/bumper latest-tag "${GITHUB_REPOSITORY}" "${GITHUB_TOKEN}")
 
     echo ::debug ::Executing bumper increment github_event_path=${GITHUB_EVENT_PATH}
-    INCREMENT=$(/bumper increment "${GITHUB_EVENT_PATH}" "${GITHUB_URL}")
+    INCREMENT=$(/bumper increment "${GITHUB_EVENT_PATH}")
 
     echo ::debug ::Executing bumper semver latest_tag=${LATEST_TAG},increment=${INCREMENT}
     NEXT_TAG=$(/bumper semver "${LATEST_TAG}" "${INCREMENT}" "${TAG_FORMAT}" --baseHost "${GITHUB_BASE_HOST}" --uploadHost "${GITHUB_UPLOAD_HOST}")
